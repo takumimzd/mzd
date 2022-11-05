f3546d06ce04444cde0163d2d6f346de1c977fb9
takumimzd <retnuh.takumi@gmail.com>
2022-11-05 20:47:14 +0900

    リファクタリング

--- src/components/features/profile/index.tsx
+++ src/components/features/profile/index.tsx
@@ -1,36 +1,21 @@
 import { Timeline, Typography } from "antd";
+import { CareerItems } from "@/assets/careerItems";
 
 const { Title, Paragraph } = Typography;
+
 export const Profile = () => {
   return (
     <div>
       <div>
         <Title level={3}>Career</Title>
         <Timeline>
-          <Timeline.Item>
-            <a href="https://stmn.co.jp/" target="_blank" rel="noreferrer">
-              stmn inc
-            </a>
-            (2021/01 - )
-          </Timeline.Item>
-          <Timeline.Item>
-            <a
-              href="https://www.resonabank.co.jp/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Risona Bank (2019/04 - 2020/08)
-            </a>
-          </Timeline.Item>
-          <Timeline.Item>
-            <a
-              href="https://www.univ.gakushuin.ac.jp/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Gakushuin University (2015/04 - 2019/03)
-            </a>
-          </Timeline.Item>
+          {CareerItems.map((item) => (
+            <Timeline.Item key={item.text}>
+              <a href={item.link} target="_blank" rel="noreferrer">
+                {item.text}
+              </a>
+            </Timeline.Item>
+          ))}
         </Timeline>
       </div>
       <div>
