1d55df241a22a2ab08ed1178a545d8530e787815
takumimzd <retnuh.takumi@gmail.com>
2022-12-24 11:22:08 +0900

    リファクタリング

--- setupEnv.ts
+++ setupEnv.ts
@@ -1,5 +1,6 @@
 import { loadEnvConfig } from "@next/env";
 
+// テスト環境でenvを使用できるようにするために使用
 const setupEnv = async (): Promise<void> => {
   const projectDir = process.cwd();
   loadEnvConfig(projectDir);
