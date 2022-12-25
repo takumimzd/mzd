7f56d568b894810bd79a470ec05d35d561ef4635
takumimzd <retnuh.takumi@gmail.com>
2022-12-25 16:07:52 +0900

    エラー時のテスト追加

--- src/features/blog/components/__tests__/OtherBlogList.test.tsx
+++ src/features/blog/components/__tests__/OtherBlogList.test.tsx
@@ -1,17 +1,31 @@
+import "@testing-library/jest-dom";
 import {
   render,
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
-import { OtherBlogListContainer } from "@/features/blog/components/ecosystems/blog-list/other-blog/other-blog-list/OtherBlogListContainer";
-import "@testing-library/jest-dom";
+import { blogsErrorHandlers } from "@/mocks/handlers/blog";
+import { server } from "@/mocks/server";
 
-test("OtherBlogListContainer", async () => {
-  render(<OtherBlogListContainer enableEdit />);
+import { OtherBlogListContainer } from "@/features/blog/components/ecosystems/blog-list/other-blog/other-blog-list/OtherBlogListContainer";
+
+test("ブログ一覧の取得に成功するとブログ一覧が表示される", async () => {
+  render(<OtherBlogListContainer enableEdit={false} />);
 
   expect(screen.getByTestId("other-blog-list-loading"));
   await waitForElementToBeRemoved(() =>
     screen.getAllByTestId("other-blog-list-loading")
   );
   expect(screen.getByTestId("other-blog-list"));
+});
+
+test("ブログ一覧の取得に失敗するとエラーメッセージが表示される", async () => {
+  server.use(...blogsErrorHandlers);
+  render(<OtherBlogListContainer enableEdit={false} />);
+
+  expect(screen.getByTestId("other-blog-list-loading"));
+  await waitForElementToBeRemoved(() =>
+    screen.getAllByTestId("other-blog-list-loading")
+  );
+  expect(screen.getByTestId("other-blog-list-error"));
 });
