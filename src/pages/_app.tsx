c38bfb143a85a1f9db3f1f4d9a0ca8eb806770b2
takumimzd <retnuh.takumi@gmail.com>
2023-06-04 20:27:37 +0900

    delete: test  Please enter the commit message for your changes. Lines starting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import "antd/dist/antd.css";
-
-import "../../styles/globals.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
